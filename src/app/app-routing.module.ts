d83e33ae8ade5a530b1940e7835d5b062d5db1ce
Huzaifa Karbalai <geohabibian@gmail.com>
2021-11-30 17:10:27 +0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductComponent } from './components/product/product.component';
+import { UserAccountComponent } from './components/user-account/user-account.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',component: HomeComponent
+  },
+  {
+    path:'products/:id', component: ProductComponent
+  },
+  {
+    path:'cart', component: CartComponent
+  },
+  {
+    path:'checkout', component: CheckoutComponent
+  },
+  {
+    path:'user-account', component: UserAccountComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CartComponent } from './components/cart/cart.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductComponent } from './components/product/product.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { UserAccountComponent } from './components/user-account/user-account.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CartComponent,
+    HomeComponent,
+    ProductComponent,
+    CheckoutComponent,
+    UserAccountComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
